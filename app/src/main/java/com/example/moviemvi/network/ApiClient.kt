7d15cbc664826852d59b87ca43fc95368c821bb4
Otabek Ibodov <101068439+itsme5695@users.noreply.github.com>
2022-05-30 14:11:44 +0500

    initial commit

---
+++ app/src/main/java/com/example/moviemvi/network/ApiClient.kt
@@ -0,0 +1,14 @@
+package com.example.moviemvi.network
+
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+
+object ApiClient {
+    private const val BASE_URL = "https://api.themoviedb.org/3/movie/"
+    private fun getRetrofit() = Retrofit.Builder()
+        .baseUrl(BASE_URL)
+        .addConverterFactory(GsonConverterFactory.create())
+        .build()
+
+    val apiService = getRetrofit().create(ApiService::class.java)
+}
